82180188c60207804ffe803a7b4439bc61977578
Suyash <suyashpathak143@gmail.com>
2020-11-28 14:27:01 +0530

    changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,10 +11,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['pk','assigned','created','status','contact','name','bmi','city','rescheduled','number']
     actions = add_admin_use.action_grabber()
 
-    def transfer_to_saurabh(self, request, queryset):
-        all=queryset.values().all().update(assigned=User.objects.get(id=4))
-
-    transfer_to_saurabh.short_description = "Transfer to Saurabh"
-
 admin.site.register(Final,OrderAdmin)
 
