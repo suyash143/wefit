9c3b44a0f4935cb08fe100609b61a322b59ac446
Suyash pathak <63799666+suyash143@users.noreply.github.com>
2020-11-30 12:48:52 +0530

    Update urls.py

--- executives/urls.py
+++ executives/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns =[
     path('register',views.employee_add,name='register'),
     path('',views.login,name='login'),
-    path('login',views.login,name='login')
+    path('login',views.login,name='login'),
+    path('logout', views.logout, name='logout')
     ]
