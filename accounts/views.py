42cb657b03088ccd1b84e8bb929285655114a242
Suyash <suyashpathak143@gmail.com>
2020-12-02 13:34:52 +0530

    functional report page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import models
+from.models import Final
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 import os
 import pytz
+from django.db import connection
 import datetime
 
 BASE = os.path.dirname(os.path.abspath(__file__))
@@ -38,7 +41,7 @@ def register(request):
                                                              height=height, gender=gender,
 
                                                              contact=mode, type=goal, insta_user=instauser, bmi=bmi,
-                                                             created=datetime.datetime.now(), status='fresh')
+                                                             created=datetime.datetime.now(o), status='fresh')
 
 
         lead3, created = models.Final.objects.get_or_create(name=name, email=email, number=number, city=city,
@@ -130,5 +133,70 @@ def edit_employee(request):
 
 
 def report(request):
-    all_data = models.Final.objects.filter(date__range=["2020-01-01", "2020-12-31"])
-    return render(request,'report.html',{'all_data':all_data})
+    if request.method=="POST":
+        startdate=request.POST.get('startdate')
+        todate=request.POST.get('todate')
+
+        all_data=models.Final.objects.raw('select * from accounts_final where created between "'+startdate+'" and "'+todate+'"')
+        all_total=0
+        all_fresh = 0
+        all_cancelled = 0
+        all_closed = 0
+        all_other = 0
+        all_rescheduled = 0
+        all_follow_up = 0
+        for all_item in all_data:
+            all_total+=1
+            if all_item.status == 'fresh':
+                all_fresh += 1
+            elif all_item.status == 'cancelled':
+                all_cancelled += 1
+            elif all_item.status == 'closed':
+                all_closed += 1
+            elif all_item.status == 'rescheduled':
+                all_rescheduled += 1
+            elif all_item.status == 'follow_up':
+                all_follow_up += 1
+            else:
+                all_other+=1
+        all={'all_total':all_total,'all_fresh':all_fresh,'all_cancelled':all_cancelled,'all_closed':all_closed,'all_rescheduled':all_rescheduled,'all_follow_up':all_follow_up}
+
+        names = User.objects.order_by('username').values('username').distinct()
+        report_name=[]
+        final={}
+        for item in names:
+            one=item['username']
+            report_name.append(one)
+            cursor = connection.cursor()
+            cursor.execute('select id from auth_user where username = "'+one+'"')
+            assigned_id=cursor.fetchall()
+            all=0
+            fresh = 0
+            cancelled = 0
+            closed = 0
+            other = 0
+            rescheduled = 0
+            follow_up = 0
+            user_data=models.Final.objects.raw('select * from accounts_final where (created between "'+startdate+'" and "'+todate+'") and assigned_id ="'+str(assigned_id[0][0])+'" ')
+            for count in user_data:
+                all+=1
+                if count.status == 'fresh':
+                    fresh += 1
+                elif count.status == 'cancelled':
+                    cancelled += 1
+                elif count.status == 'closed':
+                    closed += 1
+                elif count.status == 'rescheduled':
+                    rescheduled += 1
+                elif count.status == 'follow_up':
+                    follow_up += 1
+
+                else:
+                    other += 1
+            final[f'{one}']={'all':all,'fresh': fresh, 'cancelled': cancelled, 'closed': closed, 'rescheduled': rescheduled,'follow_up':follow_up,'other':other}
+
+
+        return render(request, 'report.html', {'all': all,'final':final,'user_data':user_data})
+    else:
+        all_data = models.Final.objects.raw('select * from accounts_final')
+        return render(request,'report.html',{'all_data':all_data})
